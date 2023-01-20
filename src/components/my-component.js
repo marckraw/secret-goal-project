66395da8e90642a8a5e2708fdc0bfc19e6f8a632
marckraw <marckraw@icloud.com>
2023-01-20 01:24:11 +0100

    Init

---
+++ src/components/my-component.js
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+const MyComponent = () => (
+    <Image
+        src="/images/profile.png"
+        height={144}
+        width={144}
+        alt="Marcin Krawczyk"
+    />
+);
